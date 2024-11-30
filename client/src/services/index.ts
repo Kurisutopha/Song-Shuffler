8e3e059ba56a786299b5fcaaef8d9acb6b95d390
angelreyes04 <angel.gardiel04@gmail.com>
2024-11-30 01:04:07 -0800

    feat: cleaned up unnecessary files from MapBox

--- client/src/services/index.ts
+++
@@ -1,5 +0,0 @@
-import { PinService } from './types/pinService';
-import { mockPinService } from './mock/mockPinService';
-
-// In 5.1, always use mock service
-export const pinService: PinService = mockPinService;
