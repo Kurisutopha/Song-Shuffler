44bc47be33bedc0b557cf1ab9577ae130ecce14e
Kurisutopha <christopher_t_williams@brown.edu>
2024-11-30 13:04:28 -0500

    feat: got the front page to load in

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,50 +1,16 @@
-import { initializeApp } from "firebase/app";
 import "../styles/App.css";
 
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignOutButton,
-  UserButton,
-} from "@clerk/clerk-react";
-import { useState, useEffect } from "react";
-// import {ProvidenceLatLong.long } from "mapbox-gl"
-
-// REMEMBER TO PUT YOUR API KEY IN A FOLDER THAT IS GITIGNORED!!
-// (for instance, /src/private/api_key.tsx)
-// import {API_KEY} from "./private/api_key"
-
-
-
 function App() {
+  console.log("App is rendering"); // Add this line to check if component is mounting
+
   return (
     <div className="App">
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-              alignContent: "center",
-              padding: "10px",
-              gap: "10px",
-            }}
-          >
-            <SignOutButton />
-            <UserButton />
-          </div>
-        </div>
-      </SignedIn>
+      <div className="app">
+        <h1 className="title">Welcome to Chime In</h1>
+        <button className="start-button">
+          Start
+        </button>
+      </div>
     </div>
   );
 }
