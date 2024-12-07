6f3568d36319bf7169929ed3a9f9c191d82f4df7
angelreyes04 <angel.gardiel04@gmail.com>
2024-12-07 17:01:24 -0500

    feat: updated handler to get the availble genres (this can help us when the user has to pick a genre for their game), and also to get the popular songs from that genre

--- client/src/components/Spotify/SpotifyHandler.ts
+++ client/src/components/Spotify/SpotifyHandler.ts
@@ -23,4 +23,21 @@ class SpotifyHandler {
             return false;
         }
     }
+
+    async getAvailableGenres() {
+        await this.initializeToken();
+        return this.spotifyApi.getAvailableGenreSeeds();
+    }
+
+    async getPopularSongsByGenre(genre: string) {
+        await this.initializeToken();
+        return await this.spotifyApi.getRecommendations({
+            seed_genres: [genre],
+            min_popularity: 70,
+            // something to think about for us:
+            // varying difficulties, if you know a lot of rock songs, and its a lot of the same popular rock songs being played, the game might be boring
+            // maybe we can have difficulty "modes", where the minimum popularity can range from like 80, 70, to maybe 50 as a "hard" mode
+            limit: 50 // API limit BTW
+        });
+    }
 }
